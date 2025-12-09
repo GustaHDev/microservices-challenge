dca787d83372f019b8f047f4231aeb962ae1a5e1
Gustavo Henrique Silva <gohv@gft.com>
2025-12-09 11:24:01 -0300

    criação da base do projeto

---
+++ exame-service/src/main/java/com/gft/exame_service/ExameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gft.exame_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExameServiceApplication.class, args);
+	}
+
+}
---
+++ exame-service/src/test/java/com/gft/exame_service/ExameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.exame_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
